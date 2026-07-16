9d5fa563077886baae932ec37fb26cfc3919d55e
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2026-07-16 22:25:28 +0800

    added better tokeniser, filter for japanese lyrics and vocabulary table

--- src/Play.jsx
+++ src/Play.jsx
@@ -1,8 +1,11 @@
-import { useParams } from "react-router-dom"
-import { useEffect, useRef, useMemo, useState } from "react";
-import "./Play.css"
+import { useParams } from "react-router-dom";
+import { useEffect, useRef, useMemo, useState, useCallback } from "react";
+import "./Play.css";
 import useSWR from "swr";
+import * as kuromoji from '@patdx/kuromoji'
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const japaneseRegex = /[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}]/u;
 const Play = () => {
     const { type, id } = useParams();
     const playerRef = useRef(null);
@@ -11,6 +14,7 @@ const Play = () => {
     const [ready, setReady] = useState(false);
     const [currentLyricI, setCurrentLyricI] = useState(0);
     const [translations, setTranslations] = useState({});
+    const [tokeniser, setTokeniser] = useState(null);
     useEffect(() => {
         if (!window.YT) {
             const tag = document.createElement('script');
@@ -88,9 +92,10 @@ const Play = () => {
             if (!strictLyricData) return null;
             if (strictLyricData.length == 0 && !lyricData) return null;
             const data = strictLyricData.length == 0 ? lyricData : strictLyricData;
-            const synced = data.find(l => l.syncedLyrics)
+            const synced = data.filter(l => l.syncedLyrics && japaneseRegex.test(l.plainLyrics))[0]
             if (!synced) return null;
-            const lines = synced.syncedLyrics.split("\n");
+            const lines = synced.syncedLyrics.split("\n").filter(l => l.length > 0 && /^\d$/.test(l[1]));
+            console.table(lines)
             const times = lines.map(line => line.split("]")[0].slice(1));
             const verses = lines.map(line => line.split("]")[1]?.trim());
             return [times, verses];
@@ -125,47 +130,98 @@ const Play = () => {
             element.scrollIntoView({ behavior: "smooth", block: "end", });
         }
     }, [currentLyricI])
-    const segment = (str) => {
-        const segmenterJa = new Intl.Segmenter("ja-JP", { granularity: "word" });
-        const segments = segmenterJa.segment(str);
-        return Array.from(segments);
-    }
-    const translate = async (e) => {
-        console.log(e.target.textContent)
-        const response = await fetch('https://api.langlyr.phyotp.dev/jisho?keyword=' + encodeURIComponent(e.target.textContent));
+    const segment = useCallback((str) => {
+        if (tokeniser){
+            const tokens = tokeniser.tokenize(str)
+            return tokens.map(t => {
+                return {segment: t.surface_form, base: t.basic_form}
+            })
+        }else{
+            const segmenterJa = new Intl.Segmenter("ja-JP", { granularity: "word" });
+            const segments = segmenterJa.segment(str);
+            return Array.from(segments).map(s => {
+                return {segment: s.segment, base: s.segment}
+            });
+        }
+    }, [tokeniser])
+    const translate = async (word) => {
+        const response = await fetch('https://api.langlyr.phyotp.dev/jisho?keyword=' + encodeURIComponent(word));
         const data = await response.json();
         const words = data?.data;
         if (words && words.length > 0) {
-            setTranslations(prev => ({...prev, [e.target.textContent]: {"meaning":words[0].senses[0].english_definitions[0],"hiragana":words[0].japanese[0].reading}}))
+            setTranslations(prev => ({...prev, [word]: {"meaning":words[0].senses[0].english_definitions[0],"hiragana": words[0].japanese[0].reading, "song": currentTitle, "sentences": [lyrics[0].filter((_,index) => lyrics[1][index].includes(word)), lyrics[1].filter((l) => l.includes(word))]}}))
         }
-        e.target.style.textDecoration = "none"
     }
+    useEffect(()=>{
+        if (!ready || tokeniser) return;
+        const initTokeniser = async () => {
+            const myLoader = {
+                async loadArrayBuffer(url) {
+                    // url = url.replace('.gz', '')
+                    const res = await fetch(
+                        '/dict/' + url, 
+                    )
+                    if (!res.ok) {
+                        throw new Error(`Failed to fetch ${url}, status: ${res.status}`)
+                    }
+                    return res.arrayBuffer()
+                },
+            }
+            const tokenizer = await new kuromoji.TokenizerBuilder({
+                loader: myLoader,
+            }).build()
+            setTokeniser(tokenizer)
+        }
+        initTokeniser()
+    },[ready])
     return (
         <div className="main">
             <div id="yt-player" />
             {ready &&
                 <div className="lyrics">
-                    {strictLyricLoading && <h1>Loading...</h1>}
-                    {lyricLoading && <h1>Still loading...</h1>}
-                    {lyrics && lyrics[1].slice(0, currentLyricI + 1).map((lyric, i) => {
+                    {!tokeniser && <p style={{position: "absolute"}}>Loading tokeniser...</p>}
+                    {strictLyricLoading && <h1>Loading lyrics...</h1>}
+                    {lyricLoading && <h1>Still loading lyrics...</h1>}
+                    {lyrics ? lyrics[1].slice(0, currentLyricI + 1).map((lyric, i) => {
                         return (<div
                             key={i}
                             ref={el => lyricRefs.current[i] = el}
                             className={`${i === currentLyricI ? "activeLyric " : ""}lyric`}
                         >
                             <p className="lyricTime">{lyrics[0][i]}</p> {segment(lyric).filter(s => s.segment.trim().length !== 0).map((s) => {
+                                const isEnglish = translations[s.base] || !japaneseRegex.test(s.segment)
                                 return (
                                     <span className="segmentContainer">
-                                        <p className="furigana">{translations[s.segment] ? translations[s.segment].hiragana : ""}</p>
-                                        <p className="segment" onClick={translations[s.segment] ? undefined : translate}>{translations[s.segment] ? translations[s.segment].meaning : s.segment}</p>
+                                        <p className="furigana">{translations[s.base] ? translations[s.base].hiragana : ""}</p>
+                                        <p className={`segment ${isEnglish ? "" : "japanese"}`} onClick={isEnglish ? undefined : () => translate(s.base)} title={s.base}>{translations[s.base] ? translations[s.base].meaning : s.segment}</p>
                                     </span>
                                 )
                             })}
                         </div>)
-                    })}
+                    }): !strictLyricLoading && !lyricLoading && lyricData && <h1>Japanese lyrics not found.</h1>}
                 </div>
-
             }
+            <div className="vocabularyTable">
+                <h1>Vocabulary</h1>
+                <table>
+                    <tr>
+                        <th>Word</th>
+                        <th>Hiragana</th>
+                        <th>Meaning</th>
+                        <th>Song</th>
+                        <th>Sentence</th>
+                    </tr>
+                    {Object.keys(translations).map(k => {
+                        return <tr key={k}>
+                            <td>{k}</td>
+                            <td>{translations[k].hiragana}</td>
+                            <td>{translations[k].meaning}</td>
+                            <td>{translations[k].song}</td>
+                            <td>{translations[k].sentences[0].join("|")}</td>
+                        </tr>
+                    })}
+                </table>
+            </div>
         </div>
 
     )
